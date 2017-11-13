7df3cac310680052f089d2d70f631c1f38091ce2
Patrick Heidotting <patrick@lakedigital.nl>
2017-11-13 19:17:33 +0100

    sonar

--- PolisAdministratie/src/main/java/nl/lakedigital/djfc/mapper/JsonMapper.java
+++ PolisAdministratie/src/main/java/nl/lakedigital/djfc/mapper/JsonMapper.java
@@ -1,7 +1,7 @@
 package nl.lakedigital.djfc.mapper;
 
 import org.springframework.stereotype.Component;
-
+@FunctionalInterface
 @Component
 public interface JsonMapper {
     boolean isVoorMij(Object object);
