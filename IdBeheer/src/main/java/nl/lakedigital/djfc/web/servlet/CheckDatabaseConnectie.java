5798d29b0b37c7a7d09f6e94f6316b3336c19fc3
Patrick Heidotting <patrick@lakedigital.nl>
2017-11-16 22:06:25 +0100

    sonar

--- IdBeheer/src/main/java/nl/lakedigital/djfc/web/servlet/CheckDatabaseConnectie.java
+++ IdBeheer/src/main/java/nl/lakedigital/djfc/web/servlet/CheckDatabaseConnectie.java
@@ -1,12 +1,8 @@
 package nl.lakedigital.djfc.web.servlet;
 
 import nl.lakedigital.djfc.repository.IdentificatieRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CheckDatabaseConnectie implements Runnable {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CheckDatabaseConnectie.class);
-
     private IdentificatieRepository identificatieRepository;
 
     public CheckDatabaseConnectie(IdentificatieRepository identificatieRepository) {
