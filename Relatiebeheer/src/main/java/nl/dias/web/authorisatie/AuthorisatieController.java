fc256649ba0a1a941e04d9691d94bbb2c898a76d
Patrick Heidotting <patrick@lakedigital.nl>
2017-11-18 22:01:36 +0100

    Token 2 uur geldig + Automatisch verlengen van een geldig token

--- Relatiebeheer/src/main/java/nl/dias/web/authorisatie/AuthorisatieController.java
+++ Relatiebeheer/src/main/java/nl/dias/web/authorisatie/AuthorisatieController.java
@@ -109,12 +109,7 @@ public class AuthorisatieController {
         try {
             Algorithm algorithm = Algorithm.HMAC512(gebruiker.getSalt());
 
-            LocalDateTime expireTime = LocalDateTime.now().plusHours(1);
-            if ("0:0:0:0:0:0:0:1".equals(httpServletRequest.getLocalAddr())) {
-                LOGGER.debug("Devomgeving, dus token met langere expire");
-                expireTime = LocalDateTime.now().plusDays(10);
-            }
-
+            LocalDateTime expireTime = LocalDateTime.now().plusHours(2);
             token = JWT.create()
                     .withSubject(gebruiker.getIdentificatie())
                     .withIssuer(httpServletRequest.getContextPath())
