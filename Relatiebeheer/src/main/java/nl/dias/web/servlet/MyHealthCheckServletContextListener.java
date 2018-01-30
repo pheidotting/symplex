7b98dc928c7d705d7d687b9b0040b953e748c3e5
Patrick Heidotting <patrick@lakedigital.nl>
2018-01-30 21:44:25 +0100

    Metrics

---
+++ Relatiebeheer/src/main/java/nl/dias/web/servlet/MyHealthCheckServletContextListener.java
@@ -0,0 +1,15 @@
+package nl.dias.web.servlet;
+
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.codahale.metrics.servlets.HealthCheckServlet;
+
+public class MyHealthCheckServletContextListener extends HealthCheckServlet.ContextListener {
+
+    public static final HealthCheckRegistry HEALTH_CHECK_REGISTRY = new HealthCheckRegistry();
+
+    @Override
+    protected HealthCheckRegistry getHealthCheckRegistry() {
+        return HEALTH_CHECK_REGISTRY;
+    }
+
+}
