fdcfb534710347d49c26c4371ce433648eda091e
Patrick Heidotting <patrick@lakedigital.nl>
2018-02-13 20:07:49 +0100

    OGA auch dabei

--- OverigeRelatieGegevensAdministratie/src/main/java/nl/lakedigital/djfc/repository/OpmerkingRepository.java
+++ OverigeRelatieGegevensAdministratie/src/main/java/nl/lakedigital/djfc/repository/OpmerkingRepository.java
@@ -1,8 +1,11 @@
 package nl.lakedigital.djfc.repository;
 
+import com.codahale.metrics.Timer;
 import nl.lakedigital.djfc.domain.Opmerking;
+import nl.lakedigital.djfc.metrics.MetricsService;
 import org.springframework.stereotype.Repository;
 
+import javax.inject.Inject;
 import java.util.List;
 
 @Repository
@@ -11,12 +14,19 @@ public class OpmerkingRepository extends AbstractRepository<Opmerking> {
         super(Opmerking.class);
     }
 
+    @Inject
+    private MetricsService metricsService;
+
     public List<Opmerking> alles() {
+        Timer.Context timer = metricsService.addTimerMetric("alles", OpmerkingRepository.class);
+
         getSession().getTransaction().begin();
 
         List<Opmerking> adressen = getSession().createQuery("select a from Opmerking a").list();
 
         getSession().getTransaction().commit();
+
+        metricsService.stop(timer);
 
         return adressen;
     }
