aa4a6aa6cd836eaccf4326696923720074359cee
Patrick Heidotting <patrick@lakedigital.nl>
2018-03-18 09:25:35 +0100

    logje

--- TestSysteem/src/test/java/nl/symplex/test/AanmeldenTest.java
+++ TestSysteem/src/test/java/nl/symplex/test/AanmeldenTest.java
@@ -18,6 +18,8 @@ public class AanmeldenTest extends AbstractTest {
 
     @Test
     public void aanmelden() {
+        System.out.println("Start test");
+        System.out.println(basisUrl);
         Inloggen inloggen = new Aanmelden().aanmelden("Fa. List Bedrog", "henkie", "jansen", "henk@heidotting.nl");
 
         inloggen.login("flb.henkie", getWachtwoord());
