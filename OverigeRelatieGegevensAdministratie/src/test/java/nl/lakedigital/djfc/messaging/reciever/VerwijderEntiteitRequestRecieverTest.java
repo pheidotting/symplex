97dfc222e8a71963e1b676691485cfc264806636
Patrick Heidotting <patrick@lakedigital.nl>
2018-04-11 14:13:10 +0200

    Coverage

---
+++ OverigeRelatieGegevensAdministratie/src/test/java/nl/lakedigital/djfc/messaging/reciever/VerwijderEntiteitRequestRecieverTest.java
@@ -0,0 +1,42 @@
+package nl.lakedigital.djfc.messaging.reciever;
+
+import nl.lakedigital.as.messaging.domain.SoortEntiteit;
+import nl.lakedigital.as.messaging.domain.SoortEntiteitEnEntiteitId;
+import nl.lakedigital.as.messaging.request.VerwijderEntiteitRequest;
+import nl.lakedigital.djfc.metrics.MetricsService;
+import nl.lakedigital.djfc.service.VerwijderEntiteitService;
+import org.easymock.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.easymock.EasyMock.expectLastCall;
+
+@RunWith(EasyMockRunner.class)
+public class VerwijderEntiteitRequestRecieverTest extends EasyMockSupport {
+    @TestSubject
+    private VerwijderEntiteitRequestReciever verwijderEntiteitRequestReciever = new VerwijderEntiteitRequestReciever();
+
+    @Mock
+    private VerwijderEntiteitService verwijderEntiteitService;
+    @Mock(MockType.NICE)
+    private MetricsService metricsService;
+
+    @Test
+    public void verwerkMessage() {
+        SoortEntiteitEnEntiteitId soortEntiteitEnEntiteitId = new SoortEntiteitEnEntiteitId();
+        soortEntiteitEnEntiteitId.setEntiteitId(6L);
+        soortEntiteitEnEntiteitId.setSoortEntiteit(SoortEntiteit.OPMERKING);
+
+        VerwijderEntiteitRequest verwijderEntiteitRequest = new VerwijderEntiteitRequest();
+        verwijderEntiteitRequest.setSoortEntiteitEnEntiteitId(soortEntiteitEnEntiteitId);
+
+        verwijderEntiteitService.verwijderen(SoortEntiteit.OPMERKING, 6L);
+        expectLastCall();
+
+        replayAll();
+
+        verwijderEntiteitRequestReciever.verwerkMessage(verwijderEntiteitRequest);
+
+        verifyAll();
+    }
+}
--- OverigeRelatieGegevensAdministratie/src/test/java/nl/lakedigital/djfc/web/controller/TelefoonnummerControllerTest.java
+++ OverigeRelatieGegevensAdministratie/src/test/java/nl/lakedigital/djfc/web/controller/TelefoonnummerControllerTest.java
@@ -94,5 +94,4 @@ public class TelefoonnummerControllerTest extends AbstractControllerTest<Telefoo
 
         verifyAll();
     }
-
 }
