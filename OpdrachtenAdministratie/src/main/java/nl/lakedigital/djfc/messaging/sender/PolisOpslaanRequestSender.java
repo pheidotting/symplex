2f3c3ef0344ac53255a34045cc1f8aa18cc17a61
Patrick Heidotting <patrick@lakedigital.nl>
2018-09-28 12:35:18 +0200

    sonar

--- OpdrachtenAdministratie/src/main/java/nl/lakedigital/djfc/messaging/sender/PolisOpslaanRequestSender.java
+++ OpdrachtenAdministratie/src/main/java/nl/lakedigital/djfc/messaging/sender/PolisOpslaanRequestSender.java
@@ -1,9 +1,6 @@
 package nl.lakedigital.djfc.messaging.sender;
 
 import nl.lakedigital.as.messaging.request.PolisOpslaanRequest;
-import nl.lakedigital.djfc.commons.domain.Opmerking;
-import nl.lakedigital.djfc.commons.domain.Pakket;
-import nl.lakedigital.djfc.commons.domain.Polis;
 import nl.lakedigital.djfc.commons.domain.SoortEntiteit;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -12,7 +9,6 @@ import javax.inject.Inject;
 import javax.jms.Destination;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
 
@@ -39,68 +35,6 @@ public class PolisOpslaanRequestSender extends AbstractSender<PolisOpslaanReques
     @Override
     public List<SoortEntiteit> getSoortEntiteiten() {
         return newArrayList(SoortEntiteit.PAKKET, SoortEntiteit.POLIS);
-    }
-
-    //    @Override
-    public PolisOpslaanRequest maakMessage(Pakket jsonPakket1) {
-        PolisOpslaanRequest polisOpslaanRequest = new PolisOpslaanRequest();
-        polisOpslaanRequest.setPokketten(newArrayList(jsonPakket1).stream().map(jsonPakket -> {
-            Pakket pakket = new Pakket();
-            pakket.setMaatschappij(jsonPakket.getMaatschappij());
-            pakket.setSoortEntiteit(jsonPakket.getSoortEntiteit());
-            pakket.setPolisNummer(jsonPakket.getPolisNummer());
-            pakket.setEntiteitId(jsonPakket.getEntiteitId());
-            pakket.setId(jsonPakket.getId());
-
-            List<Polis> polisList = jsonPakket.getPolissen().stream().map(jsonPolis -> {
-                Polis polis1 = new Polis();
-
-                polis1.setId(jsonPolis.getId());
-                //            polis1.setIdentificatie(jsonPolis.getIdentificatie());
-                // polissen die al in het systeem staan hoeven net per se een status te
-                // hebben
-                if (polis1.getStatus() != null) {
-                    polis1.setStatus(jsonPolis.getStatus());
-                }
-                polis1.setPolisNummer(jsonPolis.getPolisNummer());
-                polis1.setKenmerk(jsonPolis.getKenmerk());
-                if (jsonPolis.getPremie() != null) {
-                    polis1.setPremie(jsonPolis.getPremie());
-                }
-                polis1.setBetaalfrequentie(jsonPolis.getBetaalfrequentie());
-                polis1.setIngangsDatum(jsonPolis.getIngangsDatum());
-                polis1.setEindDatum(jsonPolis.getEindDatum());
-                polis1.setWijzigingsDatum(jsonPolis.getWijzigingsDatum());
-                polis1.setProlongatieDatum(jsonPolis.getProlongatieDatum());
-                polis1.setBetaalfrequentie(jsonPolis.getBetaalfrequentie());
-                polis1.setDekking(jsonPolis.getDekking());
-                polis1.setVerzekerdeZaak(jsonPolis.getVerzekerdeZaak());
-                polis1.setSoort(jsonPolis.getSoort());
-                polis1.setOmschrijvingVerzekering(jsonPolis.getOmschrijvingVerzekering());
-                polis1.setStatus(jsonPolis.getStatus());
-
-
-                return polis1;
-            }).collect(Collectors.toList());
-
-            pakket.setPolissen(polisList);
-            if (jsonPakket1.getOpmerkingen() != null && !jsonPakket1.getOpmerkingen().isEmpty()) {
-                pakket.setOpmerkingen(jsonPakket1.getOpmerkingen().stream().map(jsonOpmerking -> {
-                    Opmerking opmerking = new Opmerking();
-
-                    opmerking.setMedewerker(jsonOpmerking.getMedewerkerId().toString());
-                    opmerking.setOpmerking(jsonOpmerking.getOpmerking());
-                    opmerking.setTijd(jsonOpmerking.getTijd());
-                    opmerking.setIdentificatie(jsonOpmerking.getIdentificatie());
-
-                    return opmerking;
-                }).collect(Collectors.toList()));
-            }
-
-            return pakket;
-        }).collect(Collectors.toList()));
-
-        return polisOpslaanRequest;
     }
 
 }
